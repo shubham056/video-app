ade1286651c2f172131e8cc4d5cfd0f1ee83e912
Mintu Bisht <m.bisht0805@gmail.com>
2023-07-28 01:05:37 +0530

    added cors insocket

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, { cors: { origin: "*" } });
 const { v4: uuidV4 } = require('uuid');
 const userS = [], userI = [];
 const { ExpressPeerServer } = require('peer');
